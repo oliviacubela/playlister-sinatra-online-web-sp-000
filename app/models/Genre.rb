4528f7b880ad7e22fb7fdac67200d10a90bb9cf0
Olivia Cubela <oliviacubela@gmail.com>
2020-10-08 16:53:32 +0000

    Automatically backed up by Learn

--- app/models/Genre.rb
+++ app/models/Genre.rb
@@ -1,10 +1,10 @@
 require_relative "./concerns/slugifiable.rb"
 
-class Genre < ActiveRecord::Base 
-  has_many :song_genres 
-  has_many :songs, through: :song_genres 
-  has_many :artists, through: :songs 
+class Genre < ActiveRecord::Base
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
 
-  include Slugifiable::InstanceMethods 
+  include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
 end
