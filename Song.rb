8bcfa0fee87c5f62fee649d1f3d8297c7bae89bf
Olivia Cubela <oliviacubela@gmail.com>
2020-10-08 16:48:27 +0000

    Automatically backed up by Learn

--- Song.rb
+++ Song.rb
@@ -1,7 +1,7 @@
 require_relative "./concerns/slugifiable.rb"
 
-class Song < ActiveRecord::Base 
-  belongs_to :artist 
+class Song < ActiveRecord::Base
+  belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
 
